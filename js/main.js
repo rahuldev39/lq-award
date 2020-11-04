52d431173bd4e79731504e098304bcbd1fe21dd3
Rahul Dev <rahuldev.w@gmail.com>
2020-11-04 19:19:20 +0530

    notify modal added

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ jQuery(function ($) {
 
    if ($('.countdown').length > 0) {
       $(".countdown").jCounter({
-         date: '22 December 2020 12:00:00',
+         date: '26 November 2020 12:00:00',
          fallback: function () {
             console.log("count finished!")
          }
