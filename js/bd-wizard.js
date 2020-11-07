b1f7c99583351991c0b18ece7ec8d2c7b91228ff
Rahul Dev <rahuldev.w@gmail.com>
2020-11-07 23:16:15 +0530

    new wizard page added

--- js/bd-wizard.js
+++ js/bd-wizard.js
@@ -1,77 +1,119 @@
-$(document).ready(function(){
+(function($) {
 
-    var current_fs, next_fs, previous_fs; //fieldsets
-    var opacity;
-    var current = 1;
-    var steps = $("fieldset").length;
-    
-    setProgressBar(current);
-    
-    $(".next").click(function(){
-    
-    current_fs = $(this).closest("fieldset");
 
-    next_fs = $(this).closest("fieldset").next();
-    
-    //Add Class Active
-    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-    
-    //show the next fieldset
-    next_fs.show();
-    //hide the current fieldset with style
-    current_fs.animate({opacity: 0}, {
-    step: function(now) {
-    // for making fielset appear animation
-    opacity = 1 - now;
-    
-    current_fs.css({
-    'display': 'none',
-    'position': 'relative'
+
+    var form = $("#signup-form");
+    form.validate({
+        errorPlacement: function errorPlacement(error, element) {
+            element.before(error);
+        },
+        rules: {
+            email: {
+                email: true
+            }
+        },
+        onfocusout: function(element) {
+            $(element).valid();
+        },
     });
-    next_fs.css({'opacity': opacity});
-    },
-    duration: 500
+    form.children("div").steps({
+        headerTag: "h3",
+        bodyTag: "fieldset",
+        transitionEffect: "fade",
+        stepsOrientation: "vertical",
+        titleTemplate: '<div class="title"><span class="step-number">#index#</span><span class="step-text">#title#</span></div>',
+        labels: {
+            previous: 'Previous',
+            next: 'Next',
+            finish: 'Finish',
+            current: ''
+        },
+        onStepChanging: function(event, currentIndex, newIndex) {
+            if (currentIndex === 0) {
+                form.parent().parent().parent().append('<div class="footer footer-' + currentIndex + '"></div>');
+            }
+            if (currentIndex === 1) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-0').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 2) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-1').addClass('footer-' + currentIndex + '');
+            }
+            if (currentIndex === 3) {
+                form.parent().parent().parent().find('.footer').removeClass('footer-2').addClass('footer-' + currentIndex + '');
+            }
+            // if(currentIndex === 4) {
+            //     form.parent().parent().parent().append('<div class="footer" style="height:752px;"></div>');
+            // }
+            form.validate().settings.ignore = ":disabled,:hidden";
+            return form.valid();
+        },
+        onFinishing: function(event, currentIndex) {
+            form.validate().settings.ignore = ":disabled";
+            return form.valid();
+        },
+        onFinished: function(event, currentIndex) {
+            alert('Submited');
+        },
+        onStepChanged: function(event, currentIndex, priorIndex) {
+
+            return true;
+        }
     });
-    setProgressBar(++current);
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "",
+        remote: "",
+        email: "",
+        url: "",
+        date: "",
+        dateISO: "",
+        number: "",
+        digits: "",
+        creditcard: "",
+        equalTo: ""
     });
-    
-    $(".previous").click(function(){
-    
-    current_fs = $(this).closest("fieldset");
-    previous_fs = $(this).closest("fieldset").prev();
-    
-    //Remove class active
-    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-    
-    //show the previous fieldset
-    previous_fs.show();
-    
-    //hide the current fieldset with style
-    current_fs.animate({opacity: 0}, {
-    step: function(now) {
-    // for making fielset appear animation
-    opacity = 1 - now;
-    
-    current_fs.css({
-    'display': 'none',
-    'position': 'relative'
+
+    $.dobPicker({
+        daySelector: '#birth_date',
+        monthSelector: '#birth_month',
+        yearSelector: '#birth_year',
+        dayDefault: '',
+        monthDefault: '',
+        yearDefault: '',
+        minimumAge: 0,
+        maximumAge: 120
     });
-    previous_fs.css({'opacity': opacity});
-    },
-    duration: 500
-    });
-    setProgressBar(--current);
-    });
-    
-    function setProgressBar(curStep){
-    var percent = parseFloat(100 / steps) * curStep;
-    percent = percent.toFixed();
-    $(".progress-bar")
-    .css("width",percent+"%")
+    var marginSlider = document.getElementById('slider-margin');
+    if (marginSlider != undefined) {
+        noUiSlider.create(marginSlider, {
+              start: [1100],
+              step: 100,
+              connect: [true, false],
+              tooltips: [true],
+              range: {
+                  'min': 100,
+                  'max': 2000
+              },
+              pips: {
+                    mode: 'values',
+                    values: [100, 2000],
+                    density: 4
+                    },
+                format: wNumb({
+                    decimals: 0,
+                    thousand: '',
+                    prefix: '$ ',
+                })
+        });
+        var marginMin = document.getElementById('value-lower'),
+	    marginMax = document.getElementById('value-upper');
+
+        marginSlider.noUiSlider.on('update', function ( values, handle ) {
+            if ( handle ) {
+                marginMax.innerHTML = values[handle];
+            } else {
+                marginMin.innerHTML = values[handle];
+            }
+        });
     }
-    
-    $(".submit").click(function(){
-    return false;
-    })
-    
-    });
+})(jQuery);
